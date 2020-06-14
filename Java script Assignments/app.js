0030d887c3eed6e5af43102a321dda604b2d67c1
ahsansamoo <ahsansamoo550@gmail.com>
2020-06-14 19:58:49 +0500

    JavaScript Assignment

---
+++ Java script Assignments/app.js
@@ -0,0 +1,98 @@
+
+// Task No 1 Chapter No 1
+// alert("Error! Please enter a valid password");
+
+// Task no 2 Chapter No 1
+// alert('Welcome to js Land\nHappy Coding!');
+
+// Task No 3 Chapter No 1
+//   alert('Welcome to JS Land');
+//     alert('Happy Coding');
+
+// Task No 4 Chapter No 1
+// alert('Hello...I Can JS thorugh my web browser Console');
+// console.log(alert);
+
+
+// =====================================================================
+
+// Chapter No 2 Task NO 1-4
+// var Uname = "Username";
+// var fullname = "Ahsan";
+// var message = "Hello World";
+// alert('message');
+// var name = "Jhone Doe";
+// var age = "15 Years Old";
+// var com = "certified in mobile application development";
+// alert(name)
+// alert(age)
+// alert(com)
+
+// // Chapter No 2 Task NO 5
+// var name = "pizza"
+// alert ('Pizza\npizz\npiz\npi\np');
+
+// // Chapter No 2 Task NO 6
+
+// var email = "ahsansamoo550@gmail.com"
+// alert('my email address is ahsansamoo550@gmail.com');
+
+// // Chapter No 2 Task NO 7
+// var name = "book";
+// alert('i am trying to learn form book a smarter way to learn javaScript');
+
+// // Chapter No 2 Task NO 8
+// document .write ("Yah! I can HTML content through JavaScript");
+
+// // Chapter No 2 Task NO 9
+// alert ('▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬');
+
+// =====================================================================
+// Chapter No 3 Task No 1
+
+// var name = "age";
+// alert('i am 19 year old');
+
+// Chapter No 3 Task No 3
+// var name = "birthyear";
+// document .write('My birth year is 2001 <br>  Data type of my declared variable is number');
+
+// Chapter No 3 Task No 4
+
+// var visit = prompt("Visitor name")
+// var product = prompt("product name")
+// var Qty = prompt("Enter Quantity")
+// var a = (visit+Qty+product)
+// document .write(a+"Purchase on XYZ clothing")
+
+// =====================================================================
+// Chapter No 4 Task No 1
+
+// Legal & illegal variable name
+// JavaScript variables are containers for storing data values.
+
+// In this example, x, y, and z, are variables:
+
+// =====================================================================
+
+// Chapter no 5 Task 1
+
+// var a = 3;
+// var b = 5;
+// var c = 3+5;
+// document.write("sum of", + a + "and" , + b + "is" , c)
+
+// var a = 3;
+// var b = 5;
+// var c = 3-5;
+// document.write("sum of", + a + "and" , + b + "is" , c)
+
+// var a = 3;
+// var b = 5;
+// var c = 3*5;
+// document.write("sum of", + a + "and" , + b + "is" , c)
+
+var a = 3;
+var b = 5;
+var c = 3%5;
+document.write("sum of", + a + "and" , + b + "is" , c)
